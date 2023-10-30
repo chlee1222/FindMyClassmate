a20de182f158906008ddded9e1f22dfd02378900
liruan <liruan@usc.edu>
2023-10-30 13:50:56 -0700

    View Course Activity

---
+++ app/src/main/java/com/example/findmyclassmate/ViewCourse.java
@@ -0,0 +1,14 @@
+package com.example.findmyclassmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_course);
+    }
+}
