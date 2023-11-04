6c157eae4ee877664c0da8d1c0682c50726c4835
Jeffrey Lin <jeffreylin1111@gmail.com>
2023-11-04 16:01:22 -0700

    profile pages.

---
+++ app/src/main/java/com/example/findmyclassmate/edit_profile_page.java
@@ -0,0 +1,14 @@
+package com.example.findmyclassmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_profile_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_page);
+    }
+}
