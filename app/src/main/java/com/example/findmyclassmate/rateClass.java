701e9f9494446aeb64a506ac9105500c52a70cad
liruan <liruan@usc.edu>
2023-11-05 10:23:01 -0800

    Review Course Activity Front End

--- app/src/main/java/com/example/findmyclassmate/rateClass.java
+++ app/src/main/java/com/example/findmyclassmate/rateClass.java
@@ -3,12 +3,43 @@ package com.example.findmyclassmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class rateClass extends AppCompatActivity {
-
+    private EditText workloadEditText;
+    private EditText scoreEditText;
+    private CheckBox attendanceCheckBox;
+    private CheckBox lateHomeworkCheckBox;
+    private EditText commentsEditText;
+    private Button submitReviewButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rate_class);
+        workloadEditText = findViewById(R.id.workloadEditText);
+        scoreEditText = findViewById(R.id.score);
+        attendanceCheckBox = findViewById(R.id.attendanceCheckBox);
+        lateHomeworkCheckBox = findViewById(R.id.lateHomeworkCheckBox);
+        commentsEditText = findViewById(R.id.commentsEditText);
+        submitReviewButton = findViewById(R.id.submitReviewButton);
+
+        submitReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Retrieve user inputs and create a new Review object
+                String workload = workloadEditText.getText().toString();
+                float score = Float.parseFloat(scoreEditText.getText().toString());
+                boolean attendanceChecked = attendanceCheckBox.isChecked();
+                boolean lateHomeworkAllowed = lateHomeworkCheckBox.isChecked();
+                String comments = commentsEditText.getText().toString();
+
+                Review review = new Review(comments, score, attendanceChecked, lateHomeworkAllowed);
+                // Save the review to a database or other storage mechanism
+                // You need to implement a storage solution for reviews
+            }
+        });
     }
 }
