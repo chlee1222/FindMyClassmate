ac8e2a74b3d00e32c1b9d754d1fbcc544ec3f602
Jeffrey Lin <jeffreylin1111@gmail.com>
2023-11-06 12:26:08 -0800

    profile page firebase fetch

--- app/src/main/java/com/example/findmyclassmate/ProfilePage.java
+++ app/src/main/java/com/example/findmyclassmate/ProfilePage.java
@@ -1,14 +1,73 @@
 package com.example.findmyclassmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfilePage extends AppCompatActivity {
+    TextView profileUsername;
+    ImageView editPen, editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        showUserData();
+        editText.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                passUserData();
+            }
+        });
+    }
+
+    public void showUserData(){
+        Intent intent = getIntent();
+
+        String nameUser = intent.getStringExtra("name");
+
+        profileUsername.setText(nameUser);
+
+
+    }
+
+    public void passUserData(){
+        String userUsername = profileUsername.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String nameFromDB = snapshot.child(userUsername).child("name").getValue(String.class);
+
+                    Intent intent = new Intent(ProfilePage.this, edit_profile_page.class);
+                    intent.putExtra("name", nameFromDB);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
